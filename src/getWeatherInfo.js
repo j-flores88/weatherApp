ccf5f946bdf613abdafe09e2ece9c13e02adf6fb
Joel Flores <j_flores88@outlook.com>
2022-05-13 14:54:56 -0700

    change http to https

--- src/getWeatherInfo.js
+++ src/getWeatherInfo.js
@@ -6,7 +6,7 @@ import { getWeatherIcon } from "./getWeatherIcon";
 
 export const getWeatherInfo = async(location) => {
     try {
-        const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=3891df82bc1b5cf3040f45ec220c6d43&units=imperial`);
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=3891df82bc1b5cf3040f45ec220c6d43&units=imperial`);
         const localWeather = await response.json()
         const { name, weather, wind, main } = localWeather;
         let currentWeatherIcon = getWeatherIcon(weather[0].id)
