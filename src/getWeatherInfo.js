04435f00bf9898f5e6c33569fd1196379056ef68
Joel Flores <j_flores88@outlook.com>
2022-05-13 15:12:12 -0700

    move things back into promise

--- src/getWeatherInfo.js
+++ src/getWeatherInfo.js
@@ -9,21 +9,22 @@ export const getWeatherInfo = async(location) => {
     try {
         const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=3891df82bc1b5cf3040f45ec220c6d43&units=imperial`);
         const localWeather = await response.json()
-        const { name, weather, wind, main } = localWeather;
+        let { name, weather, wind, main } = localWeather;
         currentWeatherIcon = getWeatherIcon(weather[0].id);
+
+        iconDiv.innerHTML = `
+            <img src="${currentWeatherIcon}">
+        `
+        todayDiv.innerHTML = `
+            <h1>${name.toUpperCase()}</h1>
+            <h4>${weather[0].description.toUpperCase()}</h4>
+        `
+        todaysDegreeDiv.innerHTML = `
+            <h1>${Math.round(main.temp)}°</h1>
+            <h4>${Math.round(main.temp_max)}°/${Math.round(main.temp_min)}°</h4>
+        `
     } catch(err) {
         throw new Error(err)
     }
 
-    iconDiv.innerHTML = `
-        <img src="${currentWeatherIcon}">
-    `
-    todayDiv.innerHTML = `
-        <h1>${name.toUpperCase()}</h1>
-        <h4>${weather[0].description.toUpperCase()}</h4>
-    `
-    todaysDegreeDiv.innerHTML = `
-        <h1>${Math.round(main.temp)}°</h1>
-        <h4>${Math.round(main.temp_max)}°/${Math.round(main.temp_min)}°</h4>
-    `
 };
